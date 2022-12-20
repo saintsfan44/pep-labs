e0d72780cbb15de31f36582595ddd978493fc958
saintsfan44 <zyquesej@gmail.com>
2022-12-20 21:27:08 +0000

    Saved progress on Comparisons 2022-12-20 21:27:08 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -14,7 +14,10 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if(a == b) {
+            return true;
+        } else { return false;}
+       
     }
 
     /**
@@ -23,7 +26,10 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        if(a > b) {
+            return true;
+        } else {return false;}
+        
     }
     /**
      * @param a the first number to be compared.
@@ -31,7 +37,10 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if(a < b) {
+            return true;
+        } else {return false;}
+        
     }
     /**
      * @param a the first number to be compared.
@@ -39,7 +48,10 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if(a >= b) {
+            return true;
+        } else {return false;}
+        
     }
     /**
      * @param a the first number to be compared.
@@ -47,7 +59,9 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if(a <= b) {
+            return true;
+        } else {return false;}
     }
 
     /**
@@ -56,6 +70,8 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if(a != b) {
+            return true;
+        } else {return false;}
     }
 }
