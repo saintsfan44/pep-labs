830e30d06406bde7a62a85faef6607099fd7717b
saintsfan44 <zyquesej@gmail.com>
2023-01-02 03:47:00 +0000

    Saved progress on SetAllArrayElementsToN 2023-01-02 03:47:00 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -24,7 +24,7 @@ public class SetAllArrayElements {
      */
     public void set(int[] arr, int n){
         for(int i = 0; i < arr.length; i++){
-            i = n;
+           arr[i] = n;
         }
     }
 }
