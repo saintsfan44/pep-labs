7c87b3303436576bd355b1249feef4305e9ebd42
saintsfan44 <zyquesej@gmail.com>
2023-01-11 19:35:52 +0000

    Saved progress on BankAccount 2023-01-11 19:35:52 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance += amount;
     }
 
     /**
@@ -32,7 +32,12 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(balance - amount < 0) {
+            balance -= 0;
+        } else {
+            balance -= amount;
+        }
+        
     }
 
     /**
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
