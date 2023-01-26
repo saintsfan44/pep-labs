5768edcd8c3c938200e4f4f37d59f593c38a7851
saintsfan44 <zyquesej@gmail.com>
2023-01-26 15:48:33 +0000

    Saved progress on ForeignKey 2023-01-26 15:48:33 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -18,6 +20,11 @@ public class JavalinSingleton {
          */
         app.post("/problem1", ctx -> {
                 //implement logic here
+                String jsonString = ctx.body();
+
+                ObjectMapper om = new ObjectMapper();
+                Song song = om.readValue(jsonString, Song.class);
+                ctx.contentType("application/json");
         });
 
         /**
